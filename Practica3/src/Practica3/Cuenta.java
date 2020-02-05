bfb9243149cfe45ae5c36ad67df38ac25dfddbfb
Cvigara27 <cvigara@cifpfbmoll.eu>
2020-02-05 18:22:06 +0100

    Acabada practica 3

--- Practica3/src/Practica3/Cuenta.java
+++ Practica3/src/Practica3/Cuenta.java
@@ -5,33 +5,22 @@ public class Cuenta {
 	private String nombre;
 	private String numCuenta;
 	private double saldo;
-	private String usuario;
 	private String contraseña;
 	static Scanner input=new Scanner(System.in);
 	
-	public Cuenta(String nombre, String numCuenta, double saldo, String usuario, String contraseña, ArrayList<Cuenta> lista) {
+	public Cuenta(String nombre, String numCuenta, double saldo, String contraseña, ArrayList<Cuenta> lista) {
 		this.setNombre(nombre);
 		this.setNumCuenta(numCuenta);
 		this.setSaldo(saldo);
-		this.setUsuario(usuario);
 		this.setContraseña(contraseña);
 		lista.add(this);
 	}
 	
-	public Cuenta(String nombre, String numCuenta, double saldo, String usuario, String contraseña) {
+	public Cuenta(String nombre, String numCuenta, double saldo, String contraseña) {
 		this.setNombre(nombre);
 		this.setNumCuenta(numCuenta);
 		this.setSaldo(saldo);
-		this.setUsuario(usuario);
 		this.setContraseña(contraseña);
-	}
-
-	public String getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(String usuario) {
-		this.usuario = usuario;
 	}
 
 	public String getContraseña() {
@@ -39,7 +28,7 @@ public class Cuenta {
 	}
 
 	public void setContraseña(String contraseña) {
-		this.contraseña = contraseña;
+		this.contraseña = contraseña.toUpperCase();
 	}
 
 	public String getNombre() {
@@ -84,7 +73,7 @@ public class Cuenta {
 		boolean comprobar=false;
 		System.out.println("Cuanto quieres retirar?");
 		double reintegro = input.nextDouble();
-		if (reintegro<0 || reintegro>saldo) {
+		if (reintegro<0 || reintegro>this.getSaldo()) {
 			System.out.println("¡ERROR! Operación no valida");
 		} else {
 			this.setSaldo(saldo-reintegro);
@@ -93,33 +82,105 @@ public class Cuenta {
 		return comprobar;
 	}
 	
-	public void añadirPersona(ArrayList<Cuenta> lista) {
-		boolean comprobar=true;
-		int i=0;
-		while(i<lista.size() && comprobar==true){
-			System.out.print("Escribe tu usuario: ");
-			String usuario=input.nextLine();
-			if(usuario.equals(lista.get(i).getUsuario())) {
-				System.out.println("Nombre de usuario ya registrado");
-				comprobar=false;
+	public void transferir(ArrayList<Cuenta> lista) {
+		Scanner input=new Scanner(System.in);
+		boolean numcuenta=false,transfer=true;
+		int idAccount = 0;
+		while(numcuenta==false) {
+			System.out.print("Introduzca el numero de cuenta destino: ");
+			String numacc=input.nextLine();
+			int i=0;
+			while(i<lista.size() && numcuenta==false) {
+				if (numacc.equals(lista.get(i).getNumCuenta())) {
+					idAccount=i;
+					numcuenta=true;
+				}
+				i++;
 			}
-			i++;
+			if(numcuenta==false) {
+				System.out.println("El numero de cuenta no existe");
+				System.out.println("Presione ENTER para probar otra vez o escriba SALIR para finalizar el tramite");
+				String exit=input.nextLine().toUpperCase();
+				if(exit=="SALIR") {
+					numcuenta=true;
+					transfer=false;
+				}
+			}
 		}
-		System.out.print("Escribe tu contraseña: ");
-		String contraseña=input.nextLine();
+		if(transfer==true) {
+			System.out.print("Cuanto quieres transferir: ");
+			double transferir = input.nextDouble();
+			if (transferir<0 || transferir>this.getSaldo()) {
+				System.out.println("¡ERROR! Operación no valida");
+			} else {
+				this.setSaldo(saldo-transferir);
+				lista.get(idAccount).setSaldo(lista.get(idAccount).getSaldo()+transferir);
+			}
+		}
+	}
+	
+	public void verCuentas(ArrayList <Cuenta> lista) {
+		int i;
+		System.out.println("NumCuenta/Nombre/saldo");
+		for(i=0;i<lista.size();i++) {
+			System.out.println(lista.get(i).getNumCuenta()+"/"+lista.get(i).getNombre()+"/"+lista.get(i).getSaldo());
+		}
+	}
+	
+	public void añadirPersona(ArrayList<Cuenta> lista) {
+		Scanner input=new Scanner(System.in);
 		System.out.print("Escribe tu nombre: ");
 		String nombre=input.nextLine();
-		String cuenta=Integer.toString(lista.size());
-		System.out.println("Quieres hacer un ingrso inicial? [Y/N]");
+		System.out.print("Escribe tu contraseña: ");
+		String contraseña=input.nextLine();
+		System.out.println("Quieres hacer un ingreso inicial? [Y/N]");
 		String inicial=input.nextLine().toUpperCase();
-		if (inicial=="Y") {
+		double saldo=0;
+		String patata="Y";
+		if (inicial.equals(patata)) {
 			System.out.print("Cuanto vas a ingresar? ");
-			double saldo=input.nextDouble();
-		} else {
-			double saldo=0;
+			saldo=input.nextDouble();
 		}
-		Cuenta persona=new Cuenta(nombre,cuenta,saldo,usuario,contraseña);
+		String cuenta=Integer.toString(lista.size());
+		Cuenta persona=new Cuenta(nombre,cuenta,saldo,contraseña);
 		lista.add(persona);
 	}
+	
+	
+	public void cajero(ArrayList <Cuenta> lista) {
+		boolean menu=true;
+		System.out.println();
+		while(menu==true) {
+			System.out.println("---------- Cajero Automatico ----------");
+			System.out.println("|                                     |");
+			System.out.println("|   1) Ingresar dinero                |");
+			System.out.println("|   2) Retirar dinero                 |");
+			System.out.println("|   3) Realizar una trasnferencia     |");
+			System.out.println("|   4) Ver todos las cuentas          |");
+			System.out.println("|   5) Salir                          |");
+			if (this.getNumCuenta().equals("0")) {
+				System.out.println("|  9999) Crear una nueva cuenta       |");
+			}
+			System.out.println("|                                     |");
+			System.out.println("---------------------------------------");
+			System.out.println();
+			System.out.print("Que operación deseas hacer? ");
+			int opcion=input.nextInt();
+			switch(opcion) {
+				case 1:
+					this.ingresar();break;
+				case 2:
+					this.retirar();break;
+				case 3:
+					this.transferir(lista);break;
+				case 4:
+					this.verCuentas(lista);break;
+				case 5:
+					menu=false;break;
+				case 9999:
+					añadirPersona(lista);break;
+			}
+			}
+		}
 	
 }
--- Practica3/src/Practica3/Main.java
+++ Practica3/src/Practica3/Main.java
@@ -5,45 +5,60 @@ public class Main {
 	static Scanner input=new Scanner(System.in);
 	public static void main(String[] args) {
 		ArrayList<Cuenta> cuentas=new ArrayList<Cuenta>();
-		Cuenta admin=new Cuenta("Cristian","0",696969,"admin","1234",cuentas);
-		boolean login=false;
-		while (login==false) {
-			boolean user=false;boolean passw=false;
-			int count=0;
-			while (user==false && count<3) {
-				System.out.println();
-				System.out.print("Usuario: ");
-				String usuario=input.nextLine();
-				int i;
-				for (i=0; i<cuentas.size(); i++) {
-					if (usuario.equals(cuentas.get(i).getUsuario())) {
-						user=true;
+		Cuenta admin=new Cuenta("Cristian","0",696969,"1234",cuentas);
+		Cuenta admin2=new Cuenta("Don Kakas","1",2,"1234",cuentas);
+		boolean main=true;
+		String noFunciona="SALIR";
+		System.out.println("Para salir del Login escribe SALIR");
+		while (main==true) {
+			boolean login=false;
+			while (login==false) {
+				boolean account=false;boolean passw=false;
+				int count=0,idAccount=0;
+				while (account==false) {
+					System.out.println();
+					System.out.print("Nº de Cuenta: ");
+					String acc=input.nextLine().toUpperCase();
+					if (acc.equals(noFunciona)) {
+						account=true;
+						passw=true;
+						main=false;
+					}
+					int i=0;
+					boolean comprobar=false;
+					while(i<cuentas.size() && comprobar==false) {
+						if (acc.equals(cuentas.get(i).getNumCuenta())) {
+							account=true;
+							idAccount=i;
+							comprobar=true;
+						}
+						i++;
 					}
 				}
+				while (passw==false && count<3) {
+					System.out.println();
+					System.out.print("Contraseña: ");
+					String pass=input.nextLine().toUpperCase();
+					if (pass.equals(noFunciona)) {
+						passw=true;
+						main=false;
+					}
+					if (pass.equals(cuentas.get(idAccount).getContraseña())) {
+						System.out.println("Bienvenido "+cuentas.get(idAccount).getNombre());
+						cuentas.get(idAccount).cajero(cuentas);
+						passw=true;
+						login=true;
+					}
+					count++;
+				}
+				if(count==3) {
+					System.out.println("Demasiados intentos fallidos");
+				} else if (passw==true && account==true) {
+					login=true;
+				}
 			}
-		}	
-	}
-	public void cajero() {
-		boolean menu=true;
-		System.out.println();
-		while(menu==true) {
-			System.out.println();
-			System.out.println("---------- Cajero Automatico ----------");
-			System.out.println("|                                     |");
-			System.out.println("|   1) Ingresar dinero                |");
-			System.out.println("|   2) Retirar dinero                 |");
-			System.out.println("|   3) Realizar una trasnferencia     |");
-			System.out.println("|   4) Ver todos las cuentas          |");
-			System.out.println("|   5) Salir                          |");
-			System.out.println("|                                     |");
-			System.out.println("---------------------------------------");
-			System.out.println();
-			System.out.print("Que operación deseas hacer? ");
-			int opcion=input.nextInt();
-				
-			}
-		input.close();
 		}
-	
+	}
 }
+
 
